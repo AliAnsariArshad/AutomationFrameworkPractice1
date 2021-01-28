39c88912be1e3d53c6879641c13edb452cfb30c2
AliAnsariArshad <arshadali.ansari786@gmail.com>
2021-01-29 02:39:37 +0530

    Added Wedriver-manager

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
 import pytest
 from utils import utils as UT
 
@@ -12,11 +15,16 @@ def test_setup(request):
     browser = request.config.getoption("--browser")
 
     if browser == "chrome":
-        driver = webdriver.Chrome(
-            executable_path="C:\\Users\\ansarar\\SeleniumProjects\\AutomationFrameworkPractice\\drivers\\chromedriver.exe")
-    elif browser == "firefox":
-        driver = webdriver.Chrome(
-            executable_path="C:\\Users\\ansarar\\SeleniumProjects\\AutomationFrameworkPractice\\drivers\\geckodriver.exe")
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+        # driver = webdriver.Chrome(
+        #     executable_path="C:\\Users\\ansarar\\SeleniumProjects\\AutomationFrameworkPractice\\drivers\\chromedriver.exe")
+    elif browser == "ie":
+        driver = webdriver.Ie(IEDriverManager().install())
+        # driver = webdriver.Chrome(
+        #     executable_path="C:\\Users\\ansarar\\SeleniumProjects\\AutomationFrameworkPractice\\drivers\\geckodriver.exe")
+    else:
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
     driver.implicitly_wait(20)
     driver.maximize_window()
